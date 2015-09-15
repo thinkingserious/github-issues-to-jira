c8298a8549db1329ef6b57971027791f72e47f40
Elmer Thomas <elmer.thomas@sendgrid.com>
2015-09-15 09:35:23 -0700

    Jira connector can read/write

---
+++ jira_connector.py
@@ -0,0 +1,35 @@
+from jira import JIRA
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+class JiraConnector(object):
+    
+    """SendGrid GitHub Issues & Pull Requests"""
+    def __init__(self, **opts):
+        dotenv_path = join(dirname(__file__), '.env')
+        load_dotenv(dotenv_path)
+        self._username = opts.get('username', None) or os.environ.get('JIRA_USERNAME')
+        self._password = opts.get('password', None) or os.environ.get('JIRA_PASSWORD')
+        if(self._username == None or self._password == None):
+            raise BadParams("Please specify your Jira username and password. (e.g. username=your_jira_username) or use a .env file\n")
+        self._jira = JIRA('https://jira.sendgrid.net' , 
+            basic_auth=(self._username, self._password))
+        self._issues = []
+            
+    @property
+    def jira(self):
+        return self._jira
+    
+    @property
+    def issues(self):
+        return self._issues
+    
+    def get_issues(self, userid):
+        self._issues = self.jira.search_issues('assignee=' + userid)
+        return self.issues
+        
+    def add_comment(self, issue_id, comment):
+        issue = self.jira.issue(issue_id)
+        result = self.jira.add_comment(issue, comment)
+        return result
--- test.py
+++ test.py
@@ -1,11 +1,21 @@
 from app import SendGridGitHubIssues
+from jira_connector import JiraConnector
 
+"""
 repository = "sendgrid-python"
 
-client = SendGridGitHubIssues()
-issues, pulls = client.get_open_issues_and_pull_requests(repository)
+gh_client = SendGridGitHubIssues()
+issues, pulls = gh_client.get_open_issues_and_pull_requests(repository)
 print issues
 print pulls
-csv = client.create_csv()
+csv = gh_client.create_csv()
 f1=open('./csv.txt', 'w+')
 f1.write(csv)
+
+"""
+
+jira_client = JiraConnector()
+# issues = jira_client.get_issues("elmer")
+# print issues
+result = jira_client.add_comment("LIBS-146", "Testing Jira Connector Class")
+print result
